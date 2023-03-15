b20d56656e484b1e1feb8a11259ca642cf1a4502
woskoboy <woskoboy@gmail.com>
2023-03-15 16:46:46 +0000

    post

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
